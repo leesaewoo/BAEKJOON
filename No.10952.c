c7494c161613c49154d32f7f74a590784244306c
2saewoo <39489835+leesaewoo@users.noreply.github.com>
2019-03-12 23:56:18 +0900

    complete

---
+++ No.10952.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B, i;
+	
+	while(1)
+	{
+		scanf("%d %d", &A, &B);
+		if(A == 0 && B == 0)
+		{
+			break;
+		}
+		else
+		{
+			printf("%d\n", A+B);
+		}
+	}
+	
+	return 0;
+}
