56c62a31e08bf4f23d31de41ee41beb2d6f9355f
2saewoo <39489835+leesaewoo@users.noreply.github.com>
2019-03-24 15:27:45 +0900

    memory over 2

--- No.2447.c
+++ No.2447.c
@@ -1,59 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void star(int N, int x, int y, int **p);
+
+int K;
+void star(int N, int x, int y, char (*p)[K]);
+
 
 int main(void)
 {
-	int N;
-	scanf("%d", &N);
+	scanf("%d", &K);
 	
-	int **p = (int**)malloc(sizeof(int*) * N);
-	int i;
-	for(i = 0 ; i < N ; i++)
+	char p[K][K];
+	
+	int i, j;
+	for(i = 0 ; i < K ; i++)
 	{
-		*(p + i) = (int*)malloc(sizeof(int) * N);
+		for(j = 0 ; j < K ; j++)
+		{
+			p[i][j] = ' ';
+		}
 	}
 	
-	star(N, 0, 0, p);
+	star(K, 0, 0, p);
 	
-	int j;
-	for(i = 0 ; i < N ; i++)
+	for(i = 0 ; i < K ; i++)
 	{
-		for(j = 0 ; j < N ; j++)
+		for(j = 0 ; j < K ; j++)
 		{
-			if(*(*(p + i) + j) == 1)
-			{
-				printf("*");
-			}
-			else
-			{
-				printf(" ");
-			}
+			printf("%c", p[i][j]);
 		}
 		printf("\n");
-	}
-	
-	
-	for(i = 0 ; i < N ; i++)
-	{
-		free(*(p + i));
 	}
 	
 	return 0;
 }
 
-void star(int N, int x, int y, int **p)
+void star(int N, int x, int y, char (*p)[K])
 {
 	if(N == 3)
 	{
-		*(*(p + x) + y) = 1;	*(*(p + x) + y + 1) = 1; *(*(p + x) + y + 2) = 1;
-		*(*(p + x + 1) + y) = 1; *(*(p + x + 1) + y + 1) = 0; *(*(p + x + 1) + y + 2) = 1;
-		*(*(p + x + 2) + y) = 1; *(*(p + x + 2) + y + 1) = 1; *(*(p + x + 2) + y + 2) = 1;
+		p[x][y] = '*';		p[x][y + 1] = '*'; 		p[x][y + 2] = '*';
+		p[x + 1][y] = '*'; 	p[x + 1][y + 1] = ' '; 	p[x + 1][y + 2] = '*';
+		p[x + 2][y] = '*'; 	p[x + 2][y + 1] = '*'; 	p[x + 2][y + 2] = '*';
 	}
 	else
 	{
-		star(N / 3, x, y, p);	star(N / 3, x, y + N/3, p);	star(N / 3, x, y + (N * 2) / 3, p);
+		star(N / 3, x, y, p);	star(N / 3, x, y + N / 3, p);	star(N / 3, x, y + (N * 2) / 3, p);
 		star(N / 3, x + N / 3, y, p);		star(N / 3, x + N / 3, y + (N * 2) / 3, p);
 		star(N / 3, x + (N * 2) / 3, y, p);	star(N / 3, x + (N * 2) / 3, y + N / 3, p);	star(N / 3, x + (N * 2) / 3, y + (N * 2) / 3, p);
 	}
