053485d80743251e0b1e851912a8c2c888f362b7
2saewoo <39489835+leesaewoo@users.noreply.github.com>
2019-03-24 22:51:40 +0900

    complete
    https://www.acmicpc.net/problem/2447

--- No.2447.c
+++ No.2447.c
@@ -3,15 +3,14 @@
 
 
 int K;
-void star(int N, int x, int y, char (*p)[K]);
+char p[2187][2187];
+void star(int N, int x, int y);
 
 
 int main(void)
 {
 	scanf("%d", &K);
-	
-	char p[K][K];
-	
+		
 	int i, j;
 	for(i = 0 ; i < K ; i++)
 	{
@@ -21,7 +20,7 @@ int main(void)
 		}
 	}
 	
-	star(K, 0, 0, p);
+	star(K, 0, 0);
 	
 	for(i = 0 ; i < K ; i++)
 	{
@@ -35,7 +34,7 @@ int main(void)
 	return 0;
 }
 
-void star(int N, int x, int y, char (*p)[K])
+void star(int N, int x, int y)
 {
 	if(N == 3)
 	{
@@ -43,10 +42,14 @@ void star(int N, int x, int y, char (*p)[K])
 		p[x + 1][y] = '*'; 	p[x + 1][y + 1] = ' '; 	p[x + 1][y + 2] = '*';
 		p[x + 2][y] = '*'; 	p[x + 2][y + 1] = '*'; 	p[x + 2][y + 2] = '*';
 	}
+	else if(N == 1)
+	{
+		p[x][y] = '*';
+	}
 	else
 	{
-		star(N / 3, x, y, p);	star(N / 3, x, y + N / 3, p);	star(N / 3, x, y + (N * 2) / 3, p);
-		star(N / 3, x + N / 3, y, p);		star(N / 3, x + N / 3, y + (N * 2) / 3, p);
-		star(N / 3, x + (N * 2) / 3, y, p);	star(N / 3, x + (N * 2) / 3, y + N / 3, p);	star(N / 3, x + (N * 2) / 3, y + (N * 2) / 3, p);
+		star(N / 3, x, y);	star(N / 3, x, y + N / 3);	star(N / 3, x, y + (N * 2) / 3);
+		star(N / 3, x + N / 3, y);		star(N / 3, x + N / 3, y + (N * 2) / 3);
+		star(N / 3, x + (N * 2) / 3, y);	star(N / 3, x + (N * 2) / 3, y + N / 3);	star(N / 3, x + (N * 2) / 3, y + (N * 2) / 3);
 	}
 }
