0128076556b97ea6ee8a4a0550fd282095cf711a
2saewoo <39489835+leesaewoo@users.noreply.github.com>
2019-04-02 20:17:07 +0900

    continue3
    https://www.acmicpc.net/problem/2750

--- No.2750.c
+++ No.2750.c
@@ -3,10 +3,10 @@
 
 int *pa;
 void BUFFERCLEAR(void);
-void MERGE(int N, int X);
+void MERGE(int N, int X,int real_N);
 
 //TO DO//
-//1.  real N 값 함수에 매개변수로 추가해서 ptr2 == real N && max1 == 0 일 때 NULL값 넣기//
+//1. ㅋ... 
 //TO DO// 
 
 int main(void)
@@ -14,7 +14,16 @@ int main(void)
 	int N;
 	scanf("%d", &N);
 	
-	pa = malloc(sizeof(int) * N);
+	int real_N = N;
+	
+	if(N % 2 == 0)
+	{
+		pa = malloc(sizeof(int) * N);
+	}
+	else if(N % 2 == 1)
+	{
+		pa = malloc(sizeof(int) * (N + 1));
+	}
 	
 	int i;
 	for(i = 0 ; i < N ; i++)
@@ -23,7 +32,7 @@ int main(void)
 		BUFFERCLEAR;
 	}
 	
-	MERGE(N, 2);
+	MERGE(N, 2, real_N);
 	
 	for(i = 0 ; i < N ; i++)
 	{
@@ -39,13 +48,15 @@ void BUFFERCLEAR(void)
 	while(getchar() != '\n');
 }
 
-void MERGE(int N, int X)
+void MERGE(int N, int X, int real_N)
 {
+	int T[N];
 	int A[X];
 	int n;
 	int check = 0;
 	int max1, max2; // 포인터는 (X / 2) - 1 만큼만 이동 가능 
-	
+	int count = real_N;
+	int temp = X;
 	if(N % 2 == 1)
 	{
 		n = (N + 1) / 2;
@@ -68,6 +79,20 @@ void MERGE(int N, int X)
 		
 		for(j = 0 ; j < X ; j++)
 		{
+			if(count - X > 0)
+			{
+				count -= X;
+			}
+			else if(count - X < 0)
+			{
+				X = count;
+			}
+
+			if(ptr2 >= real_N)
+			{
+				check = 2;
+			}
+			
 			if(check == 1)
 			{
 				A[j] = *(pa + ptr2);
@@ -78,7 +103,7 @@ void MERGE(int N, int X)
 				A[j] = *(pa + ptr1);
 				ptr1++;
 			}
-			else if(*(pa + ptr1) < *(pa + ptr2))
+			else if(*(pa + ptr1) <= *(pa + ptr2))
 			{
 				A[j] = *(pa + ptr1);
 				
@@ -92,7 +117,7 @@ void MERGE(int N, int X)
 					check = 1;
 				}
 			}
-			else if(*(pa + ptr1) > *(pa + ptr2))
+			else if(*(pa + ptr1) >= *(pa + ptr2))
 			{
 				A[j] = *(pa + ptr2);
 				
@@ -108,14 +133,29 @@ void MERGE(int N, int X)
 			}
 		}
 		
-		for(j = 0 ; j < X ; j++)
-		{
-			*(pa + i * X) = A[j];
+		if(count - X < 0)
+		{	
+			for(j = 0 ; j < count ; j++)
+			{
+				T[i * X + j] = A[j];
+			}
 		}
+		else
+		{
+			for(j = 0 ; j < X ; j++)
+			{
+				T[i * X + j] = A[j];
+			}
+		}
+	}
+	
+	for(i = 0 ; i < real_N ; i++)
+	{
+		*(pa + i) = T[i];
 	}
 	
 	if(n != 1)
 	{
-		MERGE(N / 2, X * 2);
+		MERGE(n / 2, temp * 2, real_N);
 	}
 }
