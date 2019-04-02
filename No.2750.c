3b4904a63be6d4b5d003282adfe69ca64021ee8a
2saewoo <39489835+leesaewoo@users.noreply.github.com>
2019-04-03 00:30:06 +0900

    complete
    https://www.acmicpc.net/problem/2750

--- No.2750.c
+++ No.2750.c
@@ -2,28 +2,15 @@
 #include <stdlib.h>
 
 int *pa;
-void BUFFERCLEAR(void);
-void MERGE(int N, int X,int real_N);
-
-//TO DO//
-//1. ㅋ... 
-//TO DO// 
+void BUFFERCLEAR(void); // 버퍼 비우는 함수 
+void MERGE(int N, int X,int real_N); // MERGE 정렬 함수 
 
 int main(void)
 {
 	int N;
 	scanf("%d", &N);
-	
-	int real_N = N;
-	
-	if(N % 2 == 0)
-	{
-		pa = malloc(sizeof(int) * N);
-	}
-	else if(N % 2 == 1)
-	{
-		pa = malloc(sizeof(int) * (N + 1));
-	}
+		
+	pa = malloc(sizeof(int) * N);
 	
 	int i;
 	for(i = 0 ; i < N ; i++)
@@ -32,7 +19,7 @@ int main(void)
 		BUFFERCLEAR;
 	}
 	
-	MERGE(N, 2, real_N);
+	MERGE(N, 2, N);
 	
 	for(i = 0 ; i < N ; i++)
 	{
@@ -48,15 +35,17 @@ void BUFFERCLEAR(void)
 	while(getchar() != '\n');
 }
 
-void MERGE(int N, int X, int real_N)
+void MERGE(int N, int X, int real_N) // real_N값은 고정값(정수의 개수)
 {
-	int T[N];
-	int A[X];
-	int n;
-	int check = 0;
-	int max1, max2; // 포인터는 (X / 2) - 1 만큼만 이동 가능 
-	int count = real_N;
-	int temp = X;
+	int T[real_N]; //MERGE정렬 한번 끝날 때 최종 정렬된 값이 저장, *pa 로 값 복사 
+	int A[X]; // 정렬하면서 합쳐지는 값들이 임시 저장될 배열 
+	int n; // 정렬하면서 합쳐지는 값들이 몇 덩어리로 나눠질지 
+	int check; // 체크용 변수 
+	int max1, max2; // 포인터의 최대 이동 가능 횟수 
+	int count = real_N; // 합쳐지고 남은 자릿수를 카운트 하는 변수 
+	int temp; // 4칸에 들어갈 숫자의 개수가 칸의 수 보다 적을 때 숫자의 갯수가 X 대신 들어가는 변수 
+	int check_X; // count - X < 0 일때 1이 되는 변수, 평소엔 0 //체크용 변수임 
+	
 	if(N % 2 == 1)
 	{
 		n = (N + 1) / 2;
@@ -66,8 +55,8 @@ void MERGE(int N, int X, int real_N)
 		n = N / 2;
 	}
 	
-	int ptr1 = 0;
-	int ptr2 = 0;
+	int ptr1; // 좌측 인덱스 포인터 
+	int ptr2; // 우측 인덱스 포인터 
 	int i, j;
 	
 	for(i = 0 ; i < n ; i++)
@@ -76,18 +65,22 @@ void MERGE(int N, int X, int real_N)
 		max2 = (X / 2) - 1;
 		ptr1 = i * X;
 		ptr2 = ptr1 + (X / 2);
+		check = 0;
+		check_X = 0;
+		temp = X;
 		
-		for(j = 0 ; j < X ; j++)
+		if(count - X > 0)
 		{
-			if(count - X > 0)
-			{
-				count -= X;
-			}
-			else if(count - X < 0)
-			{
-				X = count;
-			}
-
+			count -= X;
+		}
+		else if(count - X < 0)
+		{
+			temp = count;
+			check_X = 1;
+		}
+		
+		for(j = 0 ; j < temp ; j++)
+		{
 			if(ptr2 >= real_N)
 			{
 				check = 2;
@@ -133,14 +126,14 @@ void MERGE(int N, int X, int real_N)
 			}
 		}
 		
-		if(count - X < 0)
+		if(check_X == 1)
 		{	
 			for(j = 0 ; j < count ; j++)
 			{
 				T[i * X + j] = A[j];
 			}
 		}
-		else
+		else if(check_X == 0)
 		{
 			for(j = 0 ; j < X ; j++)
 			{
@@ -154,8 +147,12 @@ void MERGE(int N, int X, int real_N)
 		*(pa + i) = T[i];
 	}
 	
-	if(n != 1)
+	if(n != 1 && n % 2 == 0)
 	{
-		MERGE(n / 2, temp * 2, real_N);
+		MERGE(n, X * 2, real_N);
+	}
+	else if(n != 1 && n % 2 == 1)
+	{
+		MERGE(n + 1, X * 2, real_N);
 	}
 }
