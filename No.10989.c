5a188d202c2bd9d39d1c98515a47b3e2d188d4fc
2saewoo <39489835+leesaewoo@users.noreply.github.com>
2019-04-07 14:39:16 +0900

    continue2
    https://www.acmicpc.net/problem/10989

--- No.10989.c
+++ No.10989.c
@@ -4,7 +4,7 @@
 
 int *pa;
 void BUFFERCLEAR(void);
-void QUICKSORT(int piv);
+void QUICKSORT(int start, int end);
 
 int main(void)
 {
@@ -27,7 +27,12 @@ int main(void)
 	}
 	// check
 	
-	QUICKSORT();
+	QUICKSORT(0, N - 1);
+	
+	for(i = 0 ; i < N ; i++)
+	{
+		printf("end *(pa + %d) = %d\n", i, *(pa + i));
+	}
 	
 	return 0;
 }
@@ -37,7 +42,85 @@ void BUFFERCLEAR(void)
 	while(getchar() != '\n');
 }
 
-void QUICKSORT(int piv)
+void QUICKSORT(int start, int end)
 {
-	
+	if(start != end)
+	{
+		int N = end - start + 1;
+		int pivot_index = start;
+		int temp;
+		int ptr_max, ptr_min;
+		int i, j;
+		
+		while(1)
+		 {
+			for(i = start ; i < end + 1 ; i++)
+			{
+			 	if(*(pa + pivot_index) < *(pa + i))
+			 	{
+			 		ptr_max = i;
+			 		printf("ptr_max = %d\n", ptr_max);
+			 		break;
+				}
+				else if(i == end)
+				{
+					ptr_max = pivot_index;
+					printf("ptr max = pivot_index : ptr_max = %d\n", pivot_index);
+				}
+			}
+			 
+			for(i = end ; i >= start ; i--)
+			{
+			 	if(*(pa + pivot_index) > *(pa + i))
+			 	{
+			 		ptr_min = i;
+			 		printf("ptr_min = %d\n", ptr_min);
+			 		break;
+				}
+				else if(i == start)
+				{
+					ptr_min = pivot_index;
+					printf("ptr_min = pivot_index : ptr_min = %d\n", pivot_index);
+				}
+			}
+			
+			if(ptr_min > ptr_max)
+			{
+			 	temp = *(pa + ptr_min);
+			 	*(pa + ptr_min) = *(pa + ptr_max);
+			 	*(pa + ptr_max) = temp;
+			 	printf("ptr_min <-> ptr_max : %d <-> %d\n",ptr_min, ptr_max); 
+			}
+			else if(ptr_min < ptr_max && pivot_index <= ptr_min)
+			{
+			 	temp = *(pa + pivot_index);
+			 	*(pa + pivot_index) = *(pa + ptr_min);
+			 	*(pa + ptr_min) = temp;
+			 	printf("pivot_index <-> ptr_min : %d <-> %d\n", pivot_index, ptr_min);
+			 	pivot_index = ptr_min;
+			 	break;
+			}
+			else
+			{
+			 	printf("else\n");
+			 	break;
+			}
+		}
+		
+		if(pivot_index - 1 >= start)
+		{
+			printf("QUICKSORT(start, pivot_index - 1) = (%d, %d)\n", start, pivot_index - 1);
+			QUICKSORT(start, pivot_index - 1);
+		}
+		
+		if(pivot_index + 1 <= end)
+		{
+			printf("QUICKSORT(pivot_index + 1 , end) = (%d, %d)\n", pivot_index + 1, end);
+			QUICKSORT(pivot_index + 1, end);
+		}
+	}
+	else if(start == end);
+	{
+		printf("start == end : %d == %d\n", start, end);//아무것도안함 
+	}
 }
