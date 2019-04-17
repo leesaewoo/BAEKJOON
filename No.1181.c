43a13a0d469831a0be5d393f8e4de31f2ea06bb3
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-04-17 23:29:31 +0900

    continue3
    https://www.acmicpc.net/problem/1181

--- No.1181.c
+++ No.1181.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* TODO (#1#): 같은 길이의 단어들 모아놓은 배열 정렬하는 함수 만들기 */
+
+
 void BufferClear()
 {
 	while(getchar() != '\n');
@@ -14,40 +17,67 @@ typedef struct Word {
 
 int main(void)
 {	
-	Word word;
-	
+	int i, j, count;
+	char str[51];
 	int N;
-	char temp[51];
-	scanf("%d", &N); BufferClear(); // 1 <= N <= 20000
+	scanf("%d", &N); BufferClear; // 1 <= N <= 20000
+	
+	Word word;
+	Word temp;
 	
 	word.word = (char**)malloc(sizeof(char*) * N);
 	word.number = (int*)malloc(sizeof(int) * N);
 	
-	*(word.number + 1) = 1;
+	temp.word = (char**)malloc(sizeof(char*) * N);
+	temp.number = (int*)malloc(sizeof(int) * N);
+	for(i = 0 ; i < 51 ; i++)
+	{
+		*(temp.word + i) = (char*)malloc(sizeof(char) * 51);
+	}
 	
-	int i, j, count;
 	for(i = 0 ; i < N ; i++)
 	{
 		count = 0;
-		scanf("%[^\n]s", &temp); BufferClear();
+		scanf("%[^\n]s", &str); BufferClear;
 		
 		for(j = 0 ; j < 51 ; j++)
 		{
-			if(temp[j] != 0)
+			if(str[j] != 0)
 			{
 				count++;
 			}
-			else if(temp[j] == 0)
+			else if(str[j] == 0)
 			{
 				break;
 			}
 		}
 		*(word.number + i) = count;
 		*(word.word + i) = (char*)malloc(sizeof(char) * (*(word.number) + 1));
-		strcpy(*(word.word + i) , temp);
+		strcpy(*(word.word + i) , str);
 		//check//
 		printf("*(word.number + %d) = %d\n*(word.word + %d) = %s\n", i, *(word.number + i), i, *(word.word + i));
 		//check//
+	}
+	
+	int ptrTemp = 0;
+	
+	for(i = 0 ; i < 51 ; i++)
+	{
+		count = 0;
+		for(j = 0 ; j < N ; j++)
+		{
+			if(*(word.number + j) == i)
+			{
+				strcpy(*(temp.word + ptrTemp), *(word.word + j));
+				count++;
+				ptrTemp++;
+			}
+			
+			for(i = 0 ; i < N ; i++)
+			{
+				
+			}
+		}
 	}
 	
 	return 0;
