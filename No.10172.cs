29033ac37e6bfa027a666c824edb720e81770ba1
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-12-15 20:41:42 +0900

    complete
    https://www.acmicpc.net/problem/10172

---
+++ No.10172.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BAEKJOON10172
+{
+    class No10172
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.Write("||_/=\\\\__|");
+            Console.ReadKey();
+        }
+    }
+}
