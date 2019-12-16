a93b3ad58e5fe7b53a14cd1677baf6efbe70e4e7
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-12-16 23:35:04 +0900

    complete
    https://www.acmicpc.net/problem/1330

---
+++ No.1330.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BAEKJOON1330
+{
+    class No1330
+    {
+        static void Main(string[] args)
+        {
+            string original = Console.ReadLine();
+
+            string[] split = original.Split(new char[] {' '}, StringSplitOptions.None);
+
+            int A = int.Parse(split[0]);
+            int B = int.Parse(split[1]);
+
+            if(A > B)
+            {
+                Console.Write(">");
+            }
+            else if(A < B)
+            {
+                Console.Write("<");
+            }
+            else
+            {
+                Console.Write("==");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
