524183c01c8ee2d961ff837783427a012c77b479
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-12-16 23:12:12 +0900

    complete
    https://www.acmicpc.net/problem/1008

---
+++ No.1008.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BAEKJOON1008
+{
+    class No1008
+    {
+        static void Main(string[] args)
+        {
+            string original = Console.ReadLine();
+
+            string[] split = original.Split(new char[] { ' ' }, StringSplitOptions.None);
+
+            decimal A = decimal.Parse(split[0]);
+            decimal B = decimal.Parse(split[1]);
+
+            Console.WriteLine("{0}", A/B);
+
+            Console.ReadKey();
+        }
+    }
+}
