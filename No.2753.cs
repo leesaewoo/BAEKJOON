1ec6451f113f2cc8e35d7465bc564da487828635
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-12-17 21:50:15 +0900

    complete
    https://www.acmicpc.net/problem/2753

---
+++ No.2753.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BAEKJOON2753
+{
+    class No2753
+    {
+        static void Main(string[] args)
+        {
+            string original = Console.ReadLine();
+
+            int year = int.Parse(original);
+
+            if(year % 400 == 0)
+            {
+                Console.Write("1");
+            }
+            else if(year % 100 == 0)
+            {
+                Console.Write("0");
+            }
+            else if(year % 4 == 0)
+            {
+                Console.Write("1");
+            }
+            else
+            {
+                Console.Write("0");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
