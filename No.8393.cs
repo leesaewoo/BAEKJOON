2d4253338e0c8e3d0469aa22490b0d1e65070014
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-12-18 00:51:18 +0900

    complete
    https://www.acmicpc.net/problem/8393

---
+++ No.8393.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BAEKJOON8393
+{
+    class No8393
+    {
+        static void Main(string[] args)
+        {
+            string original = Console.ReadLine();
+
+            int n = int.Parse(original);
+
+            Console.Write("{0}", n * (n + 1) / 2);
+
+            Console.ReadKey();
+        }
+    }
+}
