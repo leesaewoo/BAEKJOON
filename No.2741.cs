b8f3890daab13214d9a0790df0522cb05b4fe7d8
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-12-19 22:05:41 +0900

    complete
    https://www.acmicpc.net/problem/2741

---
+++ No.2741.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BAEKJOON2741
+{
+    class No2741
+    {
+        static void Main(string[] args)
+        {
+            string original = Console.ReadLine();
+
+            int N = int.Parse(original);
+
+            for(int i = 0; i < N; i++)
+            {
+                Console.WriteLine("{0}", i + 1);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
