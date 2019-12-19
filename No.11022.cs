18f0e63f00776f8d88353dbc4cc7927353453061
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-12-19 22:34:36 +0900

    complete
    https://www.acmicpc.net/problem/11022

---
+++ No.11022.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BAEKJOON11022
+{
+    class No11022
+    {
+        static void Main(string[] args)
+        {
+            string original = Console.ReadLine();
+
+            int Case = int.Parse(original);
+
+            for(int i = 0; i < Case; i++)
+            {
+                original = Console.ReadLine();
+
+                string[] split = original.Split(new char[] { ' ' }, StringSplitOptions.None);
+
+                int A = int.Parse(split[0]);
+                int B = int.Parse(split[1]);
+
+                Console.WriteLine("Case #{0}: {1} + {2} = {3}", i + 1, A, B, A + B);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
