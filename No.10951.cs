bffaeb07c04fae1acc88c1d10c92cc4acef40c5d
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-12-19 22:51:06 +0900

    complete
    https://www.acmicpc.net/problem/10951

---
+++ No.10951.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BAEKJOON10951
+{
+    class No10951
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string original = Console.ReadLine();
+
+                if (original == null)
+                {
+                    break;
+                }
+
+                string[] split = original.Split(new char[] { ' ' }, StringSplitOptions.None);
+
+                int A = int.Parse(split[0]);
+                int B = int.Parse(split[1]);
+
+                Console.WriteLine("{0}", A + B);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
