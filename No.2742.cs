7a28686036aefac26765ce776588a5e041184025
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-12-19 22:25:23 +0900

    complete
    https://www.acmicpc.net/problem/2742

---
+++ No.2742.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BAEKJOON2742
+{
+    class No2742
+    {
+        static void Main(string[] args)
+        {
+            string original = Console.ReadLine();
+
+            int N = int.Parse(original);
+
+            for(int i = N; i > 0; i--)
+            {
+                Console.WriteLine("{0}", i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
