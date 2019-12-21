ff4ade94bb4ad713f314aab9ce3ddb3a4f1cb6eb
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2019-12-21 23:52:59 +0900

    complete
    https://www.acmicpc.net/problem/10818

---
+++ No.10818.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BaekJoon10818
+{
+    class No10818
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            string[] arrayN = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.None);
+
+            int max = -1000000;
+            int min = 1000000;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (int.Parse(arrayN[i]) >= max)
+                {
+                    max = int.Parse(arrayN[i]);
+                }
+                else if (int.Parse(arrayN[i]) <= min)
+                {
+                    min = int.Parse(arrayN[i]);
+                }
+
+                if(N == 1)
+                {
+                    min = max;
+                }
+            }
+
+            Console.Write("{0} {1}", min, max);
+        }
+    }
+}
