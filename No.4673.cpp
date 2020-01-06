f85cc68a91b8ff38522ba073605819b38da43ae6
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2020-01-06 20:17:22 +0900

    https://www.acmicpc.net/problem/4673
    complete

---
+++ No.4673.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void SumOfDigit(int num, int* &chk) {
+	string str = to_string(num);
+	int sum = num;
+
+	for(int i = 0 ; i < str.size() ; i++) {
+		sum += ((int)str[i]) - 48;
+	}
+
+	if(chk[sum] > 0) {
+		return;
+	}
+	else if(sum <= 10000) {
+		chk[sum]++;
+		SumOfDigit(sum, chk);
+	}
+}
+
+int main() {
+	int* chk = new int[10001]();
+
+	for(int i = 1 ; i <= 10000 ; i++) {
+		SumOfDigit(i, chk);
+	}
+
+	for(int i = 1 ; i < 10000; i++) {
+		if(chk[i] == 0) {
+			cout << i << endl;
+		}
+	}
+
+	system("pause");
+}
