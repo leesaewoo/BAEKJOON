3e413b5e8266337674c9d06d0f59e54f6c211234
shrimplee <39489835+leesaewoo@users.noreply.github.com>
2020-03-04 17:22:05 +0900

    https://www.acmicpc.net/problem/11651
    complete

---
+++ No.11651.cpp
@@ -0,0 +1,44 @@
+//https://www.acmicpc.net/problem/11651
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct point {
+	int x;
+	int y;
+};
+
+bool compare(point a, point b) {
+	if(a.y == b.y) {
+		return a.x < b.x;
+	}
+	else {
+		return a.y < b.y;
+	}
+}
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	vector <point> vPoint(N);
+
+	int x, y;
+	for(int i = 0 ; i < N ; i++) {
+		scanf("%d %d", &x, &y);
+		vPoint[i].x = x;
+		vPoint[i].y = y;
+	}
+
+	sort(vPoint.begin(), vPoint.end(), compare);
+
+	for(int i = 0 ; i < N ; i++) {
+		printf("%d %d\n", vPoint[i].x, vPoint[i].y);
+	}
+	
+	system("pause");
+}
